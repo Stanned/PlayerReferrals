8798bc32b5eac7d34e268ad6af20fea6351d38b7
Stan Kemp <stanexe1717@gmail.com>
2021-04-06 09:21:31 +0200

    Initial Commit

---
+++ src/main/java/com/stanexe/playerreferrals/PlayerReferrals.java
@@ -0,0 +1,17 @@
+package com.stanexe.playerreferrals;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlayerReferrals extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("PlayerReferrals has been enabled.");
+
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("PlayerReferrals has been enabled.");
+    }
+}
